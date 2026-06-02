63d91dd5717a29b63721ac31120b4cce2a0efc66
Jyne2002 <jynenithesh28@gmail.com>
2026-06-02 15:27:01 +0530

    logo changes

--- frontend/src/utils/socialIcons.js
+++ frontend/src/utils/socialIcons.js
@@ -1,5 +1,5 @@
 export const getBrandMaskedIconStyle = (iconPath) => ({
-  backgroundColor: 'var(--color-brand-red)',
+  backgroundColor: 'var(--social-icon-fill)',
   WebkitMaskImage: `url(${iconPath})`,
   maskImage: `url(${iconPath})`,
   WebkitMaskRepeat: 'no-repeat',
